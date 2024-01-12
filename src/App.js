1b9b99a43ef3a224d8d4541f069a8d26cc09251b
atskotzen <atsaudio@gmail.com>
2024-01-12 13:37:07 -0300

    Adição da página Home

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
-import Sidebar from './components/Sidebar';
-import Details from './components/Details';
-import PlantingDiagram from './components/PlantingDiagram';
-import './App.css';
+import React from "react";
+import Home from './pages/Home';
+import "./App.css";
 
 function App() {
-  const [selectedSpecies, setSelectedSpecies] = useState(null);
-
-  // Add a function to set the selected species in App component
-  const handleSpeciesChange = (species) => {
-    setSelectedSpecies(species);
-  };
-
-  return (    
+  return (
     <div className="App">
-      <h3>Saf Design</h3>
-      <Sidebar onSpeciesChange={handleSpeciesChange} />
-      <div className="main-view">
-        {/* Conditionally render PlantingDiagram and Details */}
-        {selectedSpecies && (
-          <>
-            <PlantingDiagram selectedSpecies={selectedSpecies} />
-            <Details species={selectedSpecies} />
-          </>
-        )}
-      </div>
-    </div>    
+      <Home />
+    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import Details from '../components/Details';
+import PlantingDiagram from '../components/PlantingDiagram';
+
+function Home() {
+  const [selectedSpecies, setSelectedSpecies] = useState(null);
+
+  // Add a function to set the selected species in App component
+  const handleSpeciesChange = (species) => {
+    setSelectedSpecies(species);
+  };
+
+  return (    
+    <div className="App">
+      <h3>Saf Design</h3>
+      <Sidebar onSpeciesChange={handleSpeciesChange} />
+      <div className="main-view">
+        {/* Conditionally render PlantingDiagram and Details */}
+        {selectedSpecies && (
+          <>
+            <PlantingDiagram selectedSpecies={selectedSpecies} />
+            <Details species={selectedSpecies} />
+          </>
+        )}
+      </div>
+    </div>    
+  );
+};
+
+export default Home;
