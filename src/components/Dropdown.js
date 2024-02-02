6560981595fe48a765694035f5dd213ed86bf2c7
atskotzen <atsaudio@gmail.com>
2024-02-02 19:09:11 -0300

    lista vertical especies selecionadas

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSpecies } from '../context/SpeciesContext';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
+import Chip from '@mui/material/Chip';
 
 function Dropdown({ selected = [], onSelect }) {
   const { speciesList = [] } = useSpecies();
@@ -18,8 +19,18 @@ function Dropdown({ selected = [], onSelect }) {
       onChange={handleChange}
       options={speciesList}
       getOptionLabel={(option) => option.name}
+      renderTags={(value, getTagProps) =>
+        value.map((option, index) => (
+          <Chip variant="outlined" label={option.name} {...getTagProps({ index })} />
+        ))
+      }
       renderInput={(params) => (
-        <TextField {...params} label="Selecione as espécies" placeholder="Selecionar..." />
+        <TextField 
+          {...params} 
+          label="Selecione as espécies" 
+          placeholder="Selecionar..." 
+          style={{ width: '230px' }} // Define a largura fixa aqui
+        />
       )}
     />
   );
