56f79d49b64c1aee70e906aae8c36aabddf6bc98
atskotzen <atsaudio@gmail.com>
2024-02-05 00:13:32 -0300

    altera botão "Sair". Posiciona à direita da NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Button, Box } from '@mui/material';
 import logo from '../assets/logo_agroforestDAO_round.png';
+import { auth } from '../firebase'; // Importe o auth do firebase
 
 const NavBar = () => {
+  const handleLogout = () => {
+    auth.signOut() // Chame a função signOut do Firebase auth quando o botão for clicado
+      .then(() => {
+        console.log('Usuário deslogado');
+      })
+      .catch((error) => {
+        console.error('Erro ao deslogar: ', error);
+      });
+  };
+
   return (
     <AppBar position="static" style={{ backgroundColor: '#617c59' }}>
       <Toolbar>
@@ -14,9 +25,12 @@ const NavBar = () => {
           AgroforestDAO
         </Typography>
 
+        {/* Espaço entre o título e os botões */}
+        <Box sx={{ flexGrow: 1 }} />
+
         {/* Links à direita */}
-        {/* <Button color="inherit">Link 1</Button>
-        <Button color="inherit">Link 2</Button>
+        <Button color="inherit" onClick={handleLogout}>Sair</Button>
+        {/* <Button color="inherit">Link 2</Button>
         <Button color="inherit">Link 3</Button> */}
       </Toolbar>
     </AppBar>
