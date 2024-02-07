51387863afadfc1a547a215837023ab571764b04
atskotzen <atsaudio@gmail.com>
2024-02-07 06:43:27 -0300

    Ajuste de aninhamento do Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import AppRouter from "./routes/router";
+import { SpeciesProvider } from './context/SpeciesContext'; // Importe o SpeciesProvider
 
 function App() {
   return (
     <div className="App">
-      <AppRouter />
+      <SpeciesProvider> {/* Envolver a aplicação com o SpeciesProvider */}
+        <AppRouter />
+      </SpeciesProvider>
     </div>
   );
 }
