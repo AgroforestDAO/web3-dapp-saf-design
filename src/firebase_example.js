40ae9b278554c5d5745193922d369eacc3d1bfb7
atskotzen <atsaudio@gmail.com>
2024-02-08 11:43:37 -0300

    Adicionar configuraçao firebase

---
+++ src/firebase_example.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+
+// // const firebaseConfig = {
+// //   apiKey: <YOUR API KEY>,
+// //   authDomain: <YOUR AUTH DOMAIN>,
+// //   projectId: <YOUR PROJECT ID>,
+// //   storageBucket: <YOUR STORAGE BUCKET>,
+// //   messagingSenderId: <YOUR MESSAGE SENDER ID>,
+// //   appId: <YOUR APP ID>
+// // };
+
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const firestore = getFirestore(app);
