45d15cb95bbbd2150edc26feffe67b00c8d61672
Ats <atsaudio@gmail.com>
2024-03-06 19:08:53 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpKMrulmZ255eH5M-MugjANdvfrvUbPdY",
-  authDomain: "agroforestdao.firebaseapp.com",
-  projectId: "agroforestdao",
-  storageBucket: "agroforestdao.appspot.com",
-  messagingSenderId: "684267950517",
-  appId: "1:684267950517:web:d10082199a2c552c4909fc"
+  
 };
 
 const app = initializeApp(firebaseConfig);
