b383fd1bb3a13f5a584840646bfec9a479edd6be
atskotzen <atsaudio@gmail.com>
2024-04-04 03:50:42 -0300

    adiciona Tutorial Modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 // Home.js
 import React from "react";
 import ListSafs from "../components/saf/ListSafs";
+import TutorialStepper from '../components/tutorial/TutorialStepper';
 
 
 function Home() {
   return (
-    <div className="App">    
+    <div className="App"> 
+      <TutorialStepper />   
       <ListSafs />     
     </div>
   );
