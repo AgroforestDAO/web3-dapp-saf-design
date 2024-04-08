e41e7565d01e737e854b1c458d3d1efe5ecb4022
atskotzen <atsaudio@gmail.com>
2024-04-08 01:38:29 -0300

    add cacheService

--- src/components/saf/ListSafs.js
+++ src/components/saf/ListSafs.js
@@ -1,65 +1,92 @@
 // ListSafs.js
 import React, { useEffect, useState } from 'react';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, limit } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../../firebase';
-import { getCurrentUser } from '../../services/firebaseService';
+import { useAuthContext } from '../../context/AuthContext';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { getCache, setCache, removeCache } from '../../services/cacheService'; // Importe as funções de cache
 
 const ListSafs = () => {
-  const [safs, setSafs] = useState([]);
-  const navigate = useNavigate(); // Utilização do useNavigate
- 
-  useEffect(() => {
-     const fetchSafs = async () => {
-       try {
-         const currentUser = await getCurrentUser();
-         const safsRef = collection(db, 'safs');
-         const q = query(safsRef, where('createdByUID', '==', currentUser.uid));
-         
-         const querySnapshot = await getDocs(q);
-         const safsData = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
-         setSafs(safsData);
-       } catch (error) {
-         console.error("Erro ao buscar SAFs:", error);
-       }
-     };
-   
-     fetchSafs();
-  }, []);
- 
-  return (
-     <div style={{ marginTop:"81px", padding:"10px"}}>
-       <Typography variant="h5" component="div" style={{ fontFamily: "Roboto" }}>
-         Meus SAF´s
-       </Typography>
-       {safs.map((saf) => (
-         <Box 
-         component={Card} 
-         key={saf.id}
-         sx={{ marginBottom: "15px", width: "600px", cursor: "pointer", transition: "0.3s", '&:hover': { transform: "scale(1.02)", backgroundColor: "#C3E3B9" } }} 
-         onClick={() => {
-           navigate(`/details/${saf.id}`); // Navegação programática para a página de detalhes
-          }}
-         >
-           <CardContent>
-             <Typography variant="h5" component="div" style={{ fontFamily: "Roboto" }}>
-               {saf.safName}
-             </Typography>
-             <Typography color="text.secondary" style={{ fontFamily: "Roboto" }}>
-               Mentor: {saf.mentorName}
-             </Typography>
-             <Typography color="text.secondary" style={{ fontFamily: "Roboto" }}>
-               Guardião: {saf.guardianName}
-             </Typography>
-           </CardContent>
-         </Box>
-       ))}
-     </div>
-  );
- };
- 
- export default ListSafs;
+ const [safs, setSafs] = useState([]);
+ const [loading, setLoading] = useState(false);
+ const navigate = useNavigate();
+ const { user } = useAuthContext(); // Acessa o usuário logado do contexto
+
+ useEffect(() => {
+    const fetchSafs = async () => {
+      setLoading(true);
+      try {
+        if (user) {
+          const cacheKey = `safs_${user.uid}`; // Chave de cache específica para o usuário
+          const cachedSafs = getCache(cacheKey);
+          if (cachedSafs) {
+            setSafs(cachedSafs);
+          } else {
+            const safsRef = collection(db, 'safs');
+            const q = query(safsRef, where('createdByUID', '==', user.uid), limit(5)); // Limita a 5 documentos
+            
+            const querySnapshot = await getDocs(q);
+            const safsData = querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+            setSafs(safsData);
+            setCache(cacheKey, safsData); // Armazena os SAFs no cache
+          }
+        }
+      } catch (error) {
+        console.error("Erro ao buscar SAFs:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchSafs();
+ }, [user]); // Dependência do useEffect para o usuário logado
+
+ // Função para limpar o cache do usuário
+ function clearUserCache() {
+    if (user) {
+      const cacheKey = `safs_${user.uid}`;
+      removeCache(cacheKey);
+    }
+ }
+
+ return (
+    <div style={{ marginTop:"81px", padding:"10px"}}>
+      <Typography variant="h5" component="div" style={{ fontFamily: "Roboto" }}>
+        Meus SAF´s
+      </Typography>
+      {loading ? (
+        <p>Carregando SAFs...</p>
+      ) : (
+        safs.map((saf) => (
+          <Box 
+            component={Card} 
+            key={saf.id}
+            sx={{ marginBottom: "15px", width: "600px", cursor: "pointer", transition: "0.3s", '&:hover': { transform: "scale(1.02)", backgroundColor: "#C3E3B9" } }} 
+            onClick={() => {
+              navigate(`/details/${saf.id}`); // Navegação programática para a página de detalhes
+            }}
+          >
+            <CardContent>
+              <Typography variant="h5" component="div" style={{ fontFamily: "Roboto" }}>
+                {saf.safName}
+              </Typography>
+              <Typography color="text.secondary" style={{ fontFamily: "Roboto" }}>
+                Mentor: {saf.mentorName}
+              </Typography>
+              <Typography color="text.secondary" style={{ fontFamily: "Roboto" }}>
+                Guardião: {saf.guardianName}
+              </Typography>
+            </CardContent>
+          </Box>
+        ))
+      )}
+      <button onClick={clearUserCache}>Limpar Cache</button> {/* Botão para limpar o cache do usuário */}
+    </div>
+ );
+};
+
+export default ListSafs;
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,33 @@
+// AuthContext.js
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { auth } from '../firebase';
+import { setCache, removeCache } from '../services/cacheService'; // Importe as funções de cache
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
- const [user, setUser] = useState(null); 
+ const [user, setUser] = useState(null);
  const [loading, setLoading] = useState(true);
 
  useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       if (user) {
+        // Armazena os dados do usuário no cache após o login
+        setCache('currentUser', {
+          displayName: user.displayName,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          uid: user.uid,
+        });
         setUser({
           displayName: user.displayName,
           email: user.email,
           phoneNumber: user.phoneNumber,
+          uid: user.uid,
         });
       } else {
+        // Limpa o cache do usuário ao fazer logout
+        removeCache('currentUser');
         setUser();
       }
       setLoading(false);
